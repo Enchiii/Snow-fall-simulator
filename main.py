ca5eca8e227a7d76021d45875d910ebf5f315ffc
Enchiii <eryktobiasz2006@gmail.com>
2023-11-28 22:15:07 +0100

    added falling and moving on sides

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ pygame.init()
 NUM_SNOW = 150
 WIDTH = 800
 HEIGHT = 600
-speed = 2
+speed = 3
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
+cloack = pygame.time.Clock()
 
 snow_list = []
 
 for i in range(NUM_SNOW):
-    snow_list.append([random.randint(0, WIDTH), random.randint(
-        0, HEIGHT), random.randint(1, 4)])
+    snow_list.append([
+        random.randint(0, WIDTH),
+        random.randint(0, HEIGHT),
+        random.randint(1, 3),
+        random.randint(-1, 2)
+    ])
 
 
 is_running = True
@@ -30,6 +35,22 @@ while is_running:
                            (snow[0], snow[1]), snow[2])
         snow[1] += speed
 
+        snow[0] += snow[3]
+
+        if snow[1] > HEIGHT:
+            snow[0] = random.randint(0, WIDTH)
+            snow[1] = 0
+            snow[2] = random.randint(1, 4)
+            snow[3] = random.randint(-1, 2)
+
+        if snow[0] > WIDTH:
+            snow[0] = 0
+
+        if snow[0] < 0:
+            snow[0] = WIDTH
+
     pygame.display.flip()
+
+    cloack.tick(17)
 
 pygame.quit()
